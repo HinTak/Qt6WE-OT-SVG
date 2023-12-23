6b7cf1a73240382d54b2fafe22b33d09bfcafe77
Hin-Tak Leung <htl10@users.sourceforge.net>
2023-12-23 23:04:01 +0000

    One of the jumbo files

---
+++ misc/platform_jumbo_29.cc
@@ -0,0 +1,16 @@
+/* This is a Jumbo file. Don't edit. */
+
+/* Generated with merge_for_jumbo.py. */
+
+#include "../../../../../src/3rdparty/chromium/third_party/blink/renderer/platform/fonts/skia/skia_text_metrics.cc"
+#include "../../../../../src/3rdparty/chromium/third_party/blink/renderer/platform/fonts/skia/sktypeface_factory.cc"
+#include "../../../../../src/3rdparty/chromium/third_party/blink/renderer/platform/fonts/small_caps_iterator.cc"
+#include "../../../../../src/3rdparty/chromium/third_party/blink/renderer/platform/fonts/string_truncator.cc"
+#include "../../../../../src/3rdparty/chromium/third_party/blink/renderer/platform/fonts/symbols_iterator.cc"
+#include "../../../../../src/3rdparty/chromium/third_party/blink/renderer/platform/fonts/text_rendering_mode.cc"
+#include "../../../../../src/3rdparty/chromium/third_party/blink/renderer/platform/fonts/typesetting_features.cc"
+#include "../../../../../src/3rdparty/chromium/third_party/blink/renderer/platform/fonts/unicode_range_set.cc"
+#include "../../../../../src/3rdparty/chromium/third_party/blink/renderer/platform/fonts/utf16_ragel_iterator.cc"
+#include "../../../../../src/3rdparty/chromium/third_party/blink/renderer/platform/fonts/utf16_text_iterator.cc"
+#include "../../../../../src/3rdparty/chromium/third_party/blink/renderer/platform/fonts/vdmx_parser.cc"
+#include "../../../../../src/3rdparty/chromium/third_party/blink/renderer/platform/fonts/web_font_decoder.cc"
